e916813695ae36adc9c613b9046e02c7750e3adc
dnwn0311 <60522969+dnwn0311@users.noreply.github.com>
2023-11-23 13:42:25 +0900

    검색

--- Search/models.py
+++ Search/models.py
@@ -1,18 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-import os
 
-
-class Post(models.Model):
-
-    tags = models.ManyToManyField(Tag, blank=True)
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return f'/blog/tag/{self.slug}/'
+class SearchHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    query = models.CharField(max_length=200)
+    searched_at = models.DateTimeField(auto_now_add=True)
--- Search/urls.py
+++ Search/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tag/<str:slug>/', views.tag_page),
+    path('search_suggestions/', views.SearchSuggestionView.as_view()),
+    path('popular_search/', views.PopularSearchView.as_view()),
+    path('search/<str:q>/', views.PostSearch.as_view()),
+    path('search-history/', views.SearchHistoryView.as_view()),
 ]
