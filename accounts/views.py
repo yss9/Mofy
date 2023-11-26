268f758a2fe85714e3f40e66ddeaa497202dcd9c
UDyon <128572834+UDyon@users.noreply.github.com>
2023-11-26 16:41:14 +0900

    dsf

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -10,9 +12,8 @@ from django.contrib.auth.models import update_last_login
 
 from .serializers import UserSerializer
 
-from .models import User
+from .models import User, UserData
 from django.http import JsonResponse
-
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -48,8 +49,13 @@ def login(request):
     refresh = RefreshToken.for_user(user)
     update_last_login(None, user)
 
-    return Response({'refresh_token': str(refresh),
-                     'access_token': str(refresh.access_token), }, status=status.HTTP_200_OK)
+    return Response({
+        'success':  True,
+        'message': '로그인 성공',
+        #token': str(refresh.access_token),
+        'refresh_token': str(refresh),
+        'access_token': str(refresh.access_token),
+    }, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -73,7 +79,7 @@ def finduserID(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def reset_password_step1(request):
-    userID = request.data.get('userID', None)
+    userID = request.data.get('userID')
 
     if not userID:
         return JsonResponse({'error': 'userID is required'}, status=400)
@@ -81,8 +87,10 @@ def reset_password_step1(request):
     try:
         user = User.objects.get(userID=userID)
         # Store userID in the session
-        request.session['reset_user_id'] = user.id
-        return JsonResponse({'message': 'Step 1 successful'})
+        #request.session['reset_user_id'] = user.id
+        #request.session.save()
+        return JsonResponse({'message': 'Step 1 successful',
+                             'reset_user_id': user.id})
     except User.DoesNotExist:
         return JsonResponse({'error': 'User not found'}, status=404)
 
@@ -90,22 +98,24 @@ def reset_password_step1(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def reset_password_step2(request):
-    email = request.data.get('email', None)
-    name = request.data.get('name', None)
+    email = request.data.get('email')
+    name = request.data.get('name')
+    reset_user_id = request.data.get('reset_user_id')
 
     if not email or not name:
-        return JsonResponse({'error': 'email and name are required'}, status=400)
+        return JsonResponse({'error': 'email and name are required'}, status=401)
 
     # Retrieve userID from the session
-    user_id = request.session.get('reset_user_id', None)
-    if not user_id:
-        return JsonResponse({'error': 'Invalid or expired session'}, status=400)
+    #user_id = request.session.get('reset_user_id')
+    #if not user_id:
+    #    return JsonResponse({'error': 'Invalid or expired session'}, status=402)
 
     try:
-        user = User.objects.get(id=user_id, email=email, name=name)
+        user = User.objects.get(id=reset_user_id, email=email, name=name)
         # Store additional data in the session
-        request.session['reset_user_data'] = {'email': email, 'name': name}
-        return JsonResponse({'message': 'Step 2 successful'})
+       # request.session['reset_user_data'] = {'email': email, 'name': name}
+        return JsonResponse({'message': 'Step 2 successful',
+                             'reset_user_id': user.id})
     except User.DoesNotExist:
         return JsonResponse({'error': 'User not found or invalid session'}, status=404)
 
@@ -114,16 +124,17 @@ def reset_password_step2(request):
 @permission_classes([AllowAny])
 def reset_password_step3(request):
     new_password = request.data.get('new_password', None)
+    user_id = request.data.get('user_id')
 
     if not new_password:
         return JsonResponse({'error': 'new_password is required'}, status=400)
 
     # Retrieve user data from the session
-    user_data = request.session.get('reset_user_data', None)
-    user_id = request.session.get('reset_user_id', None)
+    #user_data = request.session.get('reset_user_data', None)
+    #user_id = request.session.get('reset_user_id', None)
 
-    if not user_data or not user_id:
-        return JsonResponse({'error': 'Invalid or expired session'}, status=400)
+    #if not user_data or not user_id:
+    #    return JsonResponse({'error': 'Invalid or expired session'}, status=400)
 
     try:
         user = User.objects.get(id=user_id)
@@ -132,9 +143,45 @@ def reset_password_step3(request):
         user.save()
 
         # Clear session data
-        request.session.pop('reset_user_id', None)
-        request.session.pop('reset_user_data', None)
+        #request.session.pop('reset_user_id', None)
+        #request.session.pop('reset_user_data', None)
 
         return JsonResponse({'message': 'Password reset successfully'})
     except User.DoesNotExist:
         return JsonResponse({'error': 'User not found or invalid session'}, status=404)
+
+
+
+@authentication_classes([JWTAuthentication])  # JWTAuthentication을 사용하여 토큰 검증
+@permission_classes([IsAuthenticated])  # 인증된 사용자만 접근 허용
+def getUsername(self, request):
+   user = request.user  # 인증된 사용자 객체
+   return Response({"username": user.name})
+
+@authentication_classes([JWTAuthentication])  # JWTAuthentication을 사용하여 토큰 검증
+@permission_classes([IsAuthenticated])  # 인증된 사용자만 접근 허용
+class UserInfoView(APIView):
+    def getusername(self, request):
+        user = request.user  # 인증된 사용자 객체
+        return Response({"username": user.name})
+
+
+@authentication_classes([JWTAuthentication])  # JWTAuthentication을 사용하여 토큰 검증
+@permission_classes([IsAuthenticated])  # 인증된 사용자만 접근 허용
+class UserEdit(APIView):
+    def setUserData(self, request):
+        user = request.user
+        user_data = UserData.objects.get(user=user)
+        editname = request.data.name
+        editpw = request.data.pw
+        editweight = request.data.weight
+        editheight = request.data.height
+
+        user.name = editname
+        user.set_password(editpw)
+        user_data.weight = editweight
+        user_data.height = editheight
+
+
+        return Response({"username": user.name})
+
