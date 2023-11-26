1212245b58c5e0e7aed2790e73cf7f5f94860f79
yss9 <yse2196@gmail.com>
2023-11-26 15:03:10 +0900

    진나은봐라
    도전해라!

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,10 @@
+
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import BoardDetail, BoardList, CommentDetail, LikeDetail, CommentPutDel, Report, StyleRankView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('board/', BoardList.as_view()),
@@ -8,9 +12,9 @@ urlpatterns = [
     path('board/<int:pk>/comment/', CommentDetail.as_view()),
     path('board/<int:pk>/like', LikeDetail.as_view()),
     path('board/comment/<int:pk>', CommentPutDel.as_view()),
-    path('board/<int:pk>/report', Report.as_view()),
-    path('board/stylerank', StyleRankView.as_view()),
+    path('board/<int:pk>/report',Report.as_view()),
+    path('board/stylerank',StyleRankView.as_view()),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- drfProject/settings.py
+++ drfProject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -152,3 +153,5 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
