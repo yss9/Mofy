571a2af452c327bbe22d8eb01e8e545819dce22c
UDyon <128572834+UDyon@users.noreply.github.com>
2023-11-27 19:23:15 +0900

    canRead

--- Search/urls.py
+++ Search/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('search_suggestions/', views.SearchSuggestionView.as_view()),
     path('popular_search/', views.PopularSearchView.as_view()),
-    path('search/<str:q>/', views.PostSearch.as_view()),
-    path('search-history/', views.SearchHistoryView.as_view()),
+    path('search/<str:q>/', views.PostSearchView.as_view()),
+    path('search_history/', views.SearchHistoryView.as_view()),
 ]
