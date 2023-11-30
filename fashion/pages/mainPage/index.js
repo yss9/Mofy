e4516a3d7d6df60e22030070b3963f09806fd3eb
dnwn0311 <60522969+dnwn0311@users.noreply.github.com>
2023-11-30 22:49:56 +0900

    검색

--- fashion/pages/mainPage/index.js
+++ fashion/pages/mainPage/index.js
@@ -36,8 +36,9 @@ const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cit
 export default function BoardNewPage() {
     const [weatherData, setWeatherData] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = useState(null);
     const [username, setUsername] = useState(null);
+    const [recentSearch, setRecentSearch] = useState(null);
     const [recentSearch1, setRecentSearch1] = useState([null]);
     const [recentSearch2, setRecentSearch2] = useState([null]);
     const [recentSearch3, setRecentSearch3] = useState([null]);
@@ -54,12 +55,22 @@ export default function BoardNewPage() {
     console.log(recommendSearch1)
     console.log("recommendSearch2")
     console.log(recommendSearch2)
-    console.log("recentSearch3")
+    console.log("recommendSearch3")
     console.log(recommendSearch3)
     console.log("recommendSearch4")
     console.log(recommendSearch4)
     console.log("recommendSearch5")
     console.log(recommendSearch5)
+    console.log("recentSearch")
+    console.log(recentSearch)
+    console.log("recentSearch2")
+    console.log(recentSearch2)
+    console.log("recentSearch3")
+    console.log(recentSearch3)
+    console.log("recentSearch4")
+    console.log(recentSearch4)
+    console.log("recentSearch5")
+    console.log(recentSearch5)
 
     // * 쿠키에 저장된 토큰 값(로그인 정보)을 써묵기 위해서 변수에 저장!
     // * 토큰 값은 로그인할때 아래 주석 코드대로 저장됩니당 (pages/mks/login/index.js에 가면 볼 수 있움)
@@ -108,11 +119,12 @@ export default function BoardNewPage() {
                 });
 
                 // * 여기서는 DB 설계상 받아오는 값이 딱 하나 있는게 아니고 속성이 여러개라서, 받아오는 값 중에 원하는 속성을 지정해줬슴미다 (.search_history1)
-                setRecentSearch1(response.data.search_history1);
-                setRecentSearch2(response.data.search_history2);
-                setRecentSearch3(response.data.search_history3);
-                setRecentSearch4(response.data.search_history4);
-                setRecentSearch5(response.data.search_history5);
+                setRecentSearch(response.data);
+                // setRecentSearch1(response.data.search_history1);
+                // setRecentSearch2(response.data.search_history2);
+                // setRecentSearch3(response.data.search_history3);
+                // setRecentSearch4(response.data.search_history4);
+                // setRecentSearch5(response.data.search_history5);
 
                 // * 이거도 위에 try문처럼 반복 방지를 위해 추가함미도
                 setIsRecentSearch1Loaded(true);
@@ -221,14 +233,14 @@ export default function BoardNewPage() {
 
             axios.post("http://localhost:8000/search/", { query: search }, axiosConfig)
                 .then((response) => {
-                    if (response.data.success) { // * 데이터 가져오는거 성공하면
+                    if (response.data) { // * 데이터 가져오는거 성공하면
 
                         console.log("검색 결과:");
-                        console.log(response.data.search_results);
+                        console.log(response.data);
 
                         // * 나는 받아온 데이터들 중에 search_results 속성에 있는 값을 써먹었숨미다
                         // * 일단 (response.data) 하고 로그에 뭐라고 뜨는지 확인한 다음에 원하는 속성 이름을 .속성이름 해서 추가하면 쇽샥 가져오기 가능!
-                        renderSearchResults(response.data.search_results); //
+                        renderSearchResults(response.data); //
                     } else {
                         // Handle API error
                         console.error("검색 실패: " + response.data.message);
@@ -249,7 +261,7 @@ export default function BoardNewPage() {
 
         axios.post("http://localhost:8000/search/", { query: buttonText }, axiosConfig)
             .then((response) => {
-                if (response.data.success) {
+                if (response.data) {
 
                     console.log("검색 결과:");
                     console.log(response.data.search_results);
