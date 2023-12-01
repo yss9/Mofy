6dc5d0e05e8f8f127bacc152ac29afe0c900fe69
dnwn0311 <60522969+dnwn0311@users.noreply.github.com>
2023-12-01 11:50:49 +0900

    검색 변경

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
-from multiselectfield import MultiSelectField
 
 
 class UserManager(BaseUserManager):
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from django.contrib.auth import authenticate
