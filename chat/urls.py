109dc2d57669e36fb4a5621893511165add84cbe
dnwn0311 <60522969+dnwn0311@users.noreply.github.com>
2023-12-01 16:55:11 +0900

    chat delete

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('rooms/', views.ChatRoomListCreateView.as_view(), name='chat_rooms'),
-    path('<int:room_id>/messages', views.MessageListView.as_view(), name='chat_messages'),
-]
